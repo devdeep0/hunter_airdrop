e65b273a90906a90af6b7978be3f88e886e58c58
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-31 07:35:52 +0530

    changed contract

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,6 +1,6 @@
 // Define and export your constants here
 export const CONTRACT_ADDRESSES = {
-    nftContract: "0x1CB1A5e65610AEFF2551A50f76a87a7d3fB649C6",
+    nftContract: "0xf79a06b6a2808b92d6998f878f4bcdd437c6450b",
     // You can add other contract addresses or related constants here
   };
   
