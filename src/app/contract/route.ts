01b6656fe9b5636b7f6f5f542c28f384c6a3b563
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-03-31 16:20:26 +0530

    changed env var

--- src/app/contract/route.ts
+++ src/app/contract/route.ts
@@ -3,7 +3,6 @@ import {connect} from "@/db/connectDB"
 import User from "@/Schema/UserSchema"
 import { NextRequest, NextResponse } from "next/server"
 
-import { NextApiRequest, NextApiResponse } from 'next'; // Correct types for API routes
 
 connect()
 
@@ -12,16 +11,17 @@ export async function POST(request: NextRequest , response : NextResponse){
         const reqBody = await request.json()
         const {contractaddress}=reqBody
         console.log(contractaddress);
-
+        
         const NewUser = new User({
             contractaddress
         })
         console.log("new user save", NewUser);
-
+        
         await NewUser.save()
         return NextResponse.json({ success: true, message: 'Operation successful' });
     }
     catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
+
     }
-  }
+}
